23ffbfc105411a7d1581242ff70c2b74c47563b8
Diego Morales Román <moralesromandiego@gmail.com>
2024-02-18 17:16:18 +0100

    Fixed flake.nix

--- flake.nix
+++ flake.nix
@@ -1,25 +1,28 @@
 {
-	description = "Krakens robotics software";
+  description = "A simple C++ project with SFML and gtest";
 
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-	}
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	outputs = { self, nixpkgs, flake-utils, ... }:
-		flake-utils.lib.eachDefaultSystem (
-			let
-				pkgs = import nixpkgs { inherit system; };
-			in
-			{
-				devShell = pkgs.mkShell {
-					buildInputs = with pkgs; [
-						gcc 
-						cmake
-						sfml
-						gtest
-					]
-				}
-			}
-		);
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            cmake
+            sfml
+            gtest
+          ];
+
+          # Set any environment variables or shell hooks you need for development
+        };
+
+        # You can also define a `packages` attribute if you want to build and package your application
+      });
 }
